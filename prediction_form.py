c5bc7d716aa74395f83f569246cc4af5ca6e0bd6
Namila Perera <dilan.namila@gmail.com>
2022-01-20 23:59:55 +0530

    Field list modified

--- prediction_form.py
+++ prediction_form.py
@@ -1,6 +1,6 @@
 
 from flask_wtf import FlaskForm
-from wtforms import validators, StringField, IntegerField, FloatField, SubmitField, SelectField
+from wtforms import validators, StringField, IntegerField, FloatField, SubmitField, SelectField, SelectMultipleField
 
 class PredictionForm(FlaskForm):
     age = FloatField(label ='Age', render_kw = {'class': 'form-control'})
@@ -13,39 +13,53 @@ class PredictionForm(FlaskForm):
 
     average_years_of_betel_consumption =  FloatField(label = 'Average years of betel consumption', render_kw = {'class': 'form-control'})
 
+    betel_ingrediants = SelectMultipleField(label = 'Betal Ingrediants', choices = [('none', 'None'), ('betel', 'Betel'),
+        ('arecanut', 'Arecanut'), ('lime','Lime'), ('tobacco', 'Tobacco')], render_kw = {'class': 'form-control'})
+
     daily_average_of_smoking_frequency = FloatField(label = 'Daily average of smoking frequency', render_kw = {'class': 'form-control'})
 
     average_years_of_smoking = FloatField(label ='Average year of smoking', render_kw = {'class': 'form-control'})
 
-    has_difficulty_in_mouth_opening = SelectField(label = 'Has difficulty in mouth opening', choices = [('1','Yes'),('0','No')],
-        coerce = int, render_kw = {'class': 'form-control'})
+    smoking_type = SelectMultipleField(label = 'Smoking type', choices = [('none', 'None'), ('cigarettes', 'Cigarettes'),
+        ('beedi', 'Beedi'), ('ganja','Ganja')], render_kw = {'class': 'form-control'})
+
+    alchohol_type = SelectMultipleField(label = 'Alchohol type', choices = [('none', 'None'), ('arrack', 'Arrack'),
+        ('beer', 'Beer'), ('vodka', 'Vodka')], render_kw = {'class': 'form-control'})
+
+    has_difficulty_in_mouth_opening = SelectField(label = 'Has difficulty in mouth opening', choices = [('1','Yes'),('0','No')], coerce = int, render_kw = {'class': 'form-control'})
     
     is_toungue_movement_restricted = SelectField(label = 'Is toungue movement restricted', choices = [('1','Yes'),('0','No')], coerce = int, render_kw = {'class': 'form-control'})
 
-    medical_history = SelectField(label = 'Medical History', choices = [('none','None'),
+    is_patch_present = SelectField(label = 'Is patch present', choices = [('1','Yes'),('0','No')], coerce = int, render_kw = {'class': 'form-control'})
+
+    has_depapilation_on_lips = SelectField(label = 'Has depapilation on lips', choices = [('1','Yes'),('0','No')], coerce = int, render_kw = {'class': 'form-control'})
+
+    hemoglobin_level = FloatField(label ='Hemoglobin level', render_kw = {'class': 'form-control'})
+
+    medical_history = SelectMultipleField(label = 'Medical History', choices = [('none','None'),
         ('hypertension', 'Hypertension'), ('diabetes', 'Diabetes'), ('heart_disease', 'Heart Diseases'),
         ('respiratory_disease', 'Respiratory Diseases'), ('dermatological_problem', 'Dermatological Problem'),
         ('gi_problem', 'GI Problem'), ('malnutrition_problem', 'Malnutrition Problem'), ('depression', 'Depression'),
         ('other', 'Other')], render_kw = {'class': 'form-control'})
    
-    fibrous_bands = SelectField(label = 'Fibrous Bands', choices = [('none', 'None'),
+    fibrous_bands = SelectMultipleField(label = 'Fibrous Bands', choices = [('none', 'None'),
         ('faucal_area', 'Faucal Area'), ('buccal_mucosa','Buccal Mucosa'), ('buccal_lips', 'Buccal Lips')], render_kw = {'class': 'form-control'})
    
     has_leukoplakia = SelectField(label = 'Has Leukoplakia', choices = [('1','Yes'),('0','No')], coerce = int, render_kw = {'class': 'form-control'})
    
     has_erythroplakia = SelectField(label = 'Has Erythroplakia', choices = [('1','Yes'),('0','No')], coerce = int, render_kw = {'class': 'form-control'})
-   
-    intial_dignosis = SelectField(label = "Initial Diagnosis", choices = [('osf_with_mild_to_moderate_dysplsia', 'OSF with mild to moderate dysplsia'),
-        ('osf_with_severe_dysplasia', 'OSF with severe dysplasia'), ('carcinoma_in_situ_in_bac_of_OSF', 'Carcinoma in situ in bac of OSF'), 
-        ('osf_with_cancer', 'OSF with cancer')], render_kw = {'class': 'form-control'})
-   
+
     has_candida = SelectField(label = 'Has Candida', choices = [('1','Yes'),('0','No')], coerce = int, render_kw = {'class': 'form-control'})
    
-    intial_prescription = SelectField(label = 'Initial Prescription', choices = [('mo_excercises_and_habit_intervention', 'Mouth excercises and Habit intervention'),
+    intial_dignosis = SelectMultipleField(label = "Initial Diagnosis", choices = [('none', 'None'),('osf_with_mild_to_moderate_dysplsia', 'OSF with mild to moderate dysplsia'),
+        ('osf_with_severe_dysplasia', 'OSF with severe dysplasia'), ('carcinoma_in_situ_in_bac_of_OSF', 'Carcinoma in situ in bac of OSF'), 
+        ('osf_with_cancer', 'OSF with cancer')], render_kw = {'class': 'form-control'})
+      
+    intial_prescription = SelectMultipleField(label = 'Initial Prescription', choices = [('mo_excercises_and_habit_intervention', 'Mouth excercises and Habit intervention'),
         ('steroid_intra_lesional_injection', 'Steroid Intra Lensional Injection'), ('topical_steroids', 'Topical Steroids'), 
         ('vitamins', 'Vitamins'), ('pentoxyphylline', 'Pentoxyphylline'), ('surgery', 'Surgery'), ('antifungals', 'Antifungals')], render_kw = {'class': 'form-control'})
    
-    complaints_after_initial_prescription = SelectField(label = 'Complaints after initial perception', choices = [('none', 'None'),
+    complaints_after_initial_prescription = SelectMultipleField(label = 'Complaints after initial perception', choices = [('none', 'None'),
         ('hypertension', 'Hypertension'), ('diabetes','Diabetes'), ('heart_disease', 'Heart Disease'), ('respiratory_disease', 'Respiratory Disease'), 
         ('dermatological_problem', 'Dermatological Problem'), ('gi_problem', 'GI Problem'), ('malnutrition_problem', 'Malnutrition Problem'),
         ('depression', 'Depression'), ('other', 'Other')], render_kw = {'class': 'form-control'})
--- predictor.py
+++ predictor.py
@@ -10,16 +10,22 @@ class Predictor:
     has_burning_sensation = False
     daily_average_of_betal_quids = 0
     average_years_of_betel_consumption =  0
+    betel_ingrediants = None
     daily_average_of_smoking_frequency = 0
     average_years_of_smoking = 0
+    smoking_type = None
+    alchohol_type = None
     has_difficulty_in_mouth_opening = False
     is_toungue_movement_restricted = False
+    is_patch_present = False
+    has_depapilation_on_lips = False
+    hemoglobin_level = 0
     medical_history = None
     fibrous_bands = None
     has_leukoplakia = False
     has_erythroplakia = False
-    intial_dignosis = None
     has_candida = False
+    intial_dignosis = None
     intial_prescription = None
     complaints_after_initial_prescription = None
 
@@ -32,16 +38,22 @@ class Predictor:
         self.has_burning_sensation = form_input['has_burning_sensation']
         self.daily_average_of_betal_quids = form_input['daily_average_of_betal_quids']
         self.average_years_of_betel_consumption =  form_input['average_years_of_betel_consumption']
+        self.betel_ingrediants = form_input['betel_ingrediants']
         self.daily_average_of_smoking_frequency = form_input['daily_average_of_smoking_frequency']
         self.average_years_of_smoking = form_input['average_years_of_smoking']
-        self.has_difficulty_in_mouth_opening = False
-        self.is_toungue_movement_restricted = False
+        self.smoking_type = form_input['smoking_type']
+        self.alchohol_type = form_input['alchohol_type']
+        self.has_difficulty_in_mouth_opening = form_input['has_difficulty_in_mouth_opening']
+        self.is_toungue_movement_restricted = form_input['is_toungue_movement_restricted']
+        self.is_patch_present = form_input['is_patch_present']
+        self.has_depapilation_on_lips = form_input['has_depapilation_on_lips']
+        self.hemoglobin_level = form_input['hemoglobin_level']
         self.medical_history = form_input['medical_history']
         self.fibrous_bands = form_input['fibrous_bands']
-        self.has_leukoplakia = False
-        self.has_erythroplakia = False
+        self.has_leukoplakia = form_input['has_leukoplakia']
+        self.has_erythroplakia = form_input['has_erythroplakia']
+        self.has_candida = form_input['has_candida']
         self.intial_dignosis = form_input['intial_dignosis']
-        self.has_candida = False
         self.intial_prescription = form_input['intial_prescription']
         self.complaints_after_initial_prescription = form_input['complaints_after_initial_prescription']
 
@@ -55,18 +67,29 @@ class Predictor:
             'has_burning_sensation': 'Yes' if self.has_burning_sensation == 1 else 'No',
             'daily_average_of_betal_quids': self.daily_average_of_betal_quids,
             'average_years_of_betel_consumption': self.average_years_of_betel_consumption,
+            'betel_ingrediants':  self.process_string_list(self.betel_ingrediants),
             'daily_average_of_smoking_frequency': self.daily_average_of_smoking_frequency,
             'average_years_of_smoking': self.average_years_of_smoking,
+            'smoking_type':  self.process_string_list(self.smoking_type),
+            'alchohol_type':  self.process_string_list(self.alchohol_type),
             'has_difficulty_in_mouth_opening': 'Yes' if self.has_difficulty_in_mouth_opening == 1 else 'No',
             'is_toungue_movement_restricted': 'Yes' if self.is_toungue_movement_restricted == 1 else 'No',
-            'medical_history': self.medical_history.replace('_', ' ').capitalize(),
-            'fibrous_bands': self.fibrous_bands.replace('_', ' ').capitalize(),
+            'is_patch_present': 'Yes' if self.is_patch_present == 1 else 'No',
+            'has_depapilation_on_lips': 'Yes' if self.has_depapilation_on_lips == 1 else 'No',
+            'hemoglobin_level': self.hemoglobin_level,
+            'medical_history': self.process_string_list(self.medical_history),
+            'fibrous_bands': self.process_string_list(self.fibrous_bands),
             'has_leukoplakia': 'Yes' if self.has_leukoplakia == 1 else 'No',
             'has_erythroplakia': 'Yes' if self.has_erythroplakia == 1 else 'No',
-            'intial_dignosis': self.intial_dignosis.replace('_', ' ').capitalize(),
             'has_candida': 'Yes' if self.has_candida == 1 else 'No',
-            'intial_prescription': self.intial_prescription.replace('_', ' ').capitalize(),
-            'complaints_after_initial_prescription': self.complaints_after_initial_prescription.replace('_', ' ').capitalize(),
+            'intial_dignosis': self.process_string_list(self.intial_dignosis),
+            'intial_prescription': self.process_string_list(self.intial_prescription),
+            'complaints_after_initial_prescription': self.process_string_list(self.complaints_after_initial_prescription),
         }
         return formatted_input
-        
+
+    def process_string_list(self, list):
+        if len(list) == 0:
+            return ''
+        capitalized_list = map(lambda item: item.replace('_', ' ').capitalize(), list)
+        return ', '.join(capitalized_list)
